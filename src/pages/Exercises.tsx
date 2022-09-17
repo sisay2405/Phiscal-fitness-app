1dbd898cbbb5a6ea504ced3acb0f80a5aff48f4f
sisay2405 <abrehasisay04@gmail.comgit config --global user.name sisay2405egit config --global user.email abrehasisay04@gmail.com>
2022-09-17 18:24:20 -0400

    Feature: exercise page

---
+++ src/pages/Exercises.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/system';
+
+function Exercises() {
+  /**
+   * layout
+   * [listexercises | details | new exercise]
+   */
+  return <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 3fr 1fr' }}>Box</Box>;
+}
+
+export default Exercises;
