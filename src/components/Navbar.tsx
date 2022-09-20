3270fcdaf2167f1e928f8ae7fcfee7bfb42451cb
sisay2405 <abrehasisay04@gmail.comgit config --global user.name sisay2405egit config --global user.email abrehasisay04@gmail.com>
2022-09-20 18:11:55 -0400

    updated components and clean up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
 import Logo from '../assets/images/Logo.png';
+import { StyledNavLogoLink } from '../utils/globalStyle';
 
 function Navbar() {
   return (
@@ -12,14 +13,18 @@ function Navbar() {
       sx={{ gap: { sm: '122px', xs: '40px' }, marginTop: { sm: '32px', xs: '20px' }, justifyContent: 'none' }}
       px="20px"
     >
-      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>
-        <img src={Logo} alt="Logo" style={{ width: '48px', height: '48px', margin: '0 20px' }} />
-      </Link>
-      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
-        <Link to="/">Home</Link>
-        <Link to="/exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>
-          Exercises
+      <StyledNavLogoLink>
+        <Link to="/">
+          <img src={Logo} alt="Logo" />
         </Link>
+      </StyledNavLogoLink>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <StyledNavLogoLink>
+          <Link to="/">Home</Link>
+        </StyledNavLogoLink>
+        <StyledNavLogoLink>
+          <Link to="/exercises">Exercises</Link>
+        </StyledNavLogoLink>
       </Stack>
     </Stack>
   );
